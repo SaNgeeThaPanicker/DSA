fcab5fdee02dbfead2f83f770109c7b09121ed59
SaNgeeThaPanicker <spanicker1998@gmail.com>
2024-04-12 01:08:31 +0530

    made changes in firstProgram file

--- src/main/java/org/kunalKushwaCourse/firstProgram.java
+++ src/main/java/org/kunalKushwaCourse/firstProgram.java
@@ -34,36 +34,52 @@ public class firstProgram {
 //        System.out.println("SI = "+simpleInterest);
 
 //Take in two numbers and an operator (+, -, *, /) and calculate the value. (Use if conditions)
-        System.out.println("Enter the number1");
-        int num1=scn.nextInt();
-        System.out.println("Enter the number2");
-        int num2=scn.nextInt();
-        System.out.println("Enter the operator");
-        String operator=scn.next();
-        operator = operator.trim();
-        int numResponse;
-        if(operator.equals( "+"))
-        {
-            numResponse=num1+num2;
-            System.out.println("Response of " +num1+" "+operator+" "+num2+" = "+numResponse);
+//        System.out.println("Enter the number1");
+//        int num1=scn.nextInt();
+//        System.out.println("Enter the number2");
+//        int num2=scn.nextInt();
+//        scn.nextLine();
+//        System.out.println("Enter the operator");
+//        String operator=scn.next();
+//        int numResponse;
+//        if(operator.equals( "+"))
+//        {
+//            numResponse=num1+num2;
+//            System.out.println("Response of " +num1+" "+operator+" "+num2+" = "+numResponse);
+//
+//        } else if (operator.equals("-")) {
+//            numResponse=num1-num2;
+//            System.out.println("Response of " +num1+" "+operator+" "+num2+" = "+numResponse);
+//        }
+//        else if (operator.equals("*")) {
+//            numResponse = num1 * num2;
+//            System.out.println("Response of " +num1+" "+operator+" "+num2+" = "+numResponse);
+//        }
+//        else if (operator.equals("/")) {
+//            numResponse=num1/num2;
+//            System.out.println("Response of " +num1+" "+operator+" "+num2+" = "+numResponse);
+//        }
+//        else {
+//            System.out.println("No correct operator selected");
+//        }
 
-        } else if (operator.equals("-")) {
-            numResponse=num1-num2;
-            System.out.println("Response of " +num1+" "+operator+" "+num2+" = "+numResponse);
-        }
-        else if (operator.equals("*")) {
-            numResponse = num1 * num2;
-            System.out.println("Response of " +num1+" "+operator+" "+num2+" = "+numResponse);
-        }
-        else if (operator.equals("/")) {
-            numResponse=num1/num2;
-            System.out.println("Response of " +num1+" "+operator+" "+num2+" = "+numResponse);
-        }
-        else {
-            System.out.println("No correct operator selected");
-        }
 //Take 2 numbers as input and print the largest number.
+//        System.out.println("Enter the number1");
+//        int num1=scn.nextInt();
+//        System.out.println("Enter the number2");
+//        int num2=scn.nextInt();
+//        if (num1>num2)
+//        {
+//            System.out.println("The largest number is = "+num1);
+//        }
+//        System.out.println("The largest number is = "+num2);
+
 //Input currency in rupees and output in USD.
+        System.out.println("Enter the currency");
+        float currency=scn.nextFloat();
+        double convertedCurrency = currency*0.01200;
+        System.out.println("Currency converted from rupees to USD == "+convertedCurrency);
+
 //To calculate Fibonacci Series up to n numbers.
 //To find out whether the given String is Palindrome or not.
 //To find Armstrong Number between two given number.
